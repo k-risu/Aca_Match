2b0cc35b442179e3ec4557065f77f993e2404f29
k-risu <kkd4869@gmail.com>
2025-01-21 21:35:00 +0900

    [feat] 로그인 CSS 추가 및 일부 페이지 오류 수정

--- src/components/CustomInput .tsx
+++ src/components/CustomInput .tsx
@@ -1,29 +1,37 @@
+import { css } from "@emotion/react";
 import { Input } from "antd";
+import styled from "@emotion/styled";
 
 interface CustomInputProps {
   placeholder?: string;
   height?: string;
   width?: string;
   borderRadius?: string;
-  children?: React.ReactNode; // children prop 추가
+  children?: React.ReactNode;
   padding?: string;
   borderColor?: string;
-  focusOutline?: string; // 추가
-  focusBorder?: string; // 추가
-  border?: string; // 추가
+  focusOutline?: string;
+  focusBorder?: string;
+  border?: string;
+  type?: "text" | "password"; // type 속성 추가
+  value?: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 /**
  * 커스텀 입력 필드 컴포넌트
- * @param placeholder - (선택) 입력 필드의 플레이스홀더 텍스트
- * @param height - (선택) 입력 필드의 높이 (기본값: "56px")
- * @param width - (선택) 입력 필드의 너비 (기본값: "448px")
- * @param borderRadius - (선택) 입력 필드의 모서리 둥글기 (기본값: "12px")
- * @param padding - (선택) 입력 필드의 내부 여백
- * @param children - (선택) 입력 필드와 함께 렌더링될 자식 요소
- * @param focusOutline - (선택) 포커스 시 외곽선 스타일
- * @param focusBorder - (선택) 포커스 시 테두리 스타일
- * @param border - (선택) 기본 테두리 스타일
- * @returns 커스터마이즈된 입력 필드 컴포넌트
+ * @param {object} props - 컴포넌트 props
+ * @param {string} [props.placeholder] - (선택) 입력 필드의 placeholder 텍스트
+ * @param {string} [props.height="56px"] - (선택) 입력 필드의 높이, 기본값: 56px
+ * @param {string} [props.width="448px"] - (선택) 입력 필드의 너비, 기본값: 448px
+ * @param {string} [props.borderRadius="12px"] - (선택) 입력 필드의 테두리 둥글기, 기본값: 12px
+ * @param {string} [props.padding] - (선택) 입력 필드의 내부 여백
+ * @param {React.ReactNode} [props.children] - (선택) 자식 컴포넌트
+ * @param {string} [props.borderColor] - (선택) 테두리 색상
+ * @param {string} [props.focusOutline] - (선택) 포커스 시 외곽선 스타일
+ * @param {string} [props.focusBorder] - (선택) 포커스 시 테두리 스타일
+ * @param {string} [props.border] - (선택) 기본 테두리 스타일
+ * @param {'text' | 'password'} [props.type="text"] - (선택) 입력 필드 타입 (일반 텍스트 또는 비밀번호), 기본값: text
+ * @returns {JSX.Element} 커스텀 스타일이 적용된 입력 필드
  */
 function CustomInput({
   placeholder,
@@ -35,21 +43,60 @@ function CustomInput({
   focusOutline,
   focusBorder,
   border,
+  type = "text", // 기본값은 text
+  value,
+  onChange,
 }: CustomInputProps) {
+  const inputClassName = [
+    "text-[14px]",
+    "placeholder:text-[14px]",
+    "placeholder:text-brand-placeholder",
+    focusOutline && `focus:outline-${focusOutline}`,
+    focusBorder && `focus:border-${focusBorder}`,
+    border && `border-${border}`,
+  ]
+    .filter(Boolean)
+    .join(" ");
+  const InputStyle = styled.div`
+    .ant-input-password input::placeholder {
+      color: #507a95 !important; /* 플레이스홀더 색상 */
+      opacity: 1;
+    }
+  `;
+
   return (
     <>
-      <Input
-        className={`text-[16px] placeholder-brand-placeholder focus:outline-${focusOutline} focus:border-${focusBorder} border-${border}`}
-        placeholder={placeholder}
-        style={{
-          width: `${width}`,
-          height: `${height}`,
-          borderRadius: `${borderRadius}`,
-          padding: `${padding}`,
-          fontSize: "14px",
-        }}
-      />
-      {children} {/* children 렌더링 */}
+      {type === "password" ? (
+        <InputStyle>
+          <Input.Password
+            maxLength={16}
+            className={inputClassName}
+            placeholder={placeholder}
+            value={value}
+            onChange={onChange}
+            style={{
+              width,
+              height,
+              borderRadius,
+              padding,
+              fontSize: "14px",
+            }}
+          />
+        </InputStyle>
+      ) : (
+        <Input
+          maxLength={20}
+          className={inputClassName}
+          placeholder={placeholder}
+          style={{
+            width,
+            height,
+            borderRadius,
+            padding,
+          }}
+        />
+      )}
+      {children}
     </>
   );
 }
--- src/pages/member/LoginPage.tsx
+++ src/pages/member/LoginPage.tsx
@@ -1,74 +1,114 @@
-import React, { useState } from "react";
-import CustomModal from "../../components/modal/Modal";
+import { Checkbox, Form } from "antd";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import CustomInput from "../../components/CustomInput ";
-import { Button, Form, Input } from "antd";
+import MainButton from "../../components/button/MainButton";
 
 function LoginPage() {
   const [isModalVisible, setIsModalVisible] = useState(false);
-
-  const handleButton1Click = () => {
-    setIsModalVisible(false);
-  };
-
-  const handleButton2Click = () => {
-    setIsModalVisible(false);
-  };
-
+  const navigate = useNavigate();
   const [form] = Form.useForm();
 
   const onFinish = (values: any) => {
-    console.log("Submitted values:", values);
-  };
-
-  const handleButtonClick = () => {
-    const values = form.getFieldsValue();
-    console.log("Current values:", values);
+    console.log(values);
   };
 
   return (
     <div>
-      <button onClick={() => setIsModalVisible(true)}>모달 테스트</button>
-      <CustomModal
-        visible={isModalVisible}
-        title={"테스트"}
-        content={"작동 중"}
-        onButton1Click={handleButton1Click}
-        onButton2Click={handleButton2Click}
-        button1Text={"취소"}
-        button2Text={"확인"}
-        modalWidth={400}
-        modalHeight={244}
-      />
-      <CustomInput placeholder="입력해주세요" />
-      <div>
-        <Form form={form} onFinish={onFinish} layout="vertical">
-          <Form.Item
-            label="Name"
-            name="name"
-            rules={[{ required: true, message: "Name is required" }]}
+      <header className="sticky top-0 left-0 right-0 z-50 flex items-center h-[64px] bg-white border-b border-brand-BTWhite">
+        <div className="w-[1280px] flex items-center justify-between mx-auto  ">
+          <div
+            className="w-[210px] h-[40px] cursor-pointer mr-[full]"
+            onClick={() => {
+              navigate("/");
+            }}
           >
-            <Input />
-          </Form.Item>
+            로고
+          </div>
+        </div>
+      </header>
+      <Form
+        form={form}
+        onFinish={onFinish}
+        className="flex flex-col justify-center mx-auto "
+      >
+        <main className="flex justify-center items-center">
+          <div className="flex flex-col items-center px-5 py-6 w-full max-w-[960px]">
+            {/* 로그인 Title */}
+            <div className="flex flex-col items-center px-4 py-3 w-full h-[60px]">
+              <p className="text-center text-text-dark text-2xl font-[600]">
+                로그인
+              </p>
+            </div>
 
-          <Form.Item
-            label="Email"
-            name="email"
-            rules={[
-              { required: true, type: "email", message: "Invalid email" },
-            ]}
-          >
-            <Input />
-          </Form.Item>
+            <div className="flex flex-col h-[272px]">
+              {/* 이메일 필드 */}
+              <label className="flex text-[16px] w-[120px] font-[500] mb-[8px]">
+                이메일 &nbsp;
+                <label className="text-[#D9534F]">*</label>
+              </label>
+              <CustomInput placeholder="이메일을 입력해주세요" width="448px" />
 
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form>
+              {/* 비밀번호 */}
+              <label className="flex text-[16px] w-[120px] font-[500] my-[8px]">
+                비밀번호 &nbsp;
+                <label className="text-[#D9534F]">*</label>
+              </label>
+              <CustomInput
+                type="password"
+                placeholder="비밀번호를 입력해주세요"
+              />
 
-        <Button onClick={handleButtonClick} className="mt-4">
-          Log Current Form Values
-        </Button>
-      </div>
+              {/* 아이디 기억하기 */}
+              <div className="flex items-center justify-between mt-[8px]">
+                <Checkbox value="remember" id="remember-checkbox">
+                  <label
+                    htmlFor="remember-checkbox"
+                    className="text-[14px] text-brand-default whitespace-nowrap"
+                  >
+                    아이디 기억하기
+                  </label>
+                </Checkbox>
+                <span
+                  className="text-right text-text-gray text-sm mt-2 whitespace-nowrap cursor-pointer"
+                  onClick={() => {
+                    navigate("/");
+                  }}
+                >
+                  이메일 / 비밀번호를 잊으셨나요?
+                </span>
+              </div>
+            </div>
+
+            {/* 회원가입 버튼 */}
+            {/* <MainButton
+              text="회원가입"
+              onClick={() => {
+                navigate("/signup");
+              }}
+            /> */}
+
+            {/* SNS 로그인 */}
+            <div className="flex justify-center items-center p-2 w-full h-[56px] bg-bg-light rounded-xl mt-4">
+              <span className="text-text-dark font-semibold text-sm">
+                Log in with Kakao
+              </span>
+            </div>
+
+            {/* Google 로그인 */}
+            <div className="flex justify-center items-center p-2 w-full h-[56px] bg-bg-light rounded-xl mt-4">
+              <span className="text-text-dark font-semibold text-sm">
+                Log in with Google
+              </span>
+            </div>
+
+            {/* 로그인 버튼 */}
+            <div className="flex justify-center items-center p-2 w-full h-[56px] bg-btn-blue rounded-xl mt-4">
+              <span className="text-white font-semibold text-sm">로그인</span>
+            </div>
+          </div>
+        </main>
+      </Form>
     </div>
   );
 }
